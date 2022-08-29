d6dc6c43ace0c9657760c5431dfb098343737ba7
riiduu <jelinsksk@gmail.com>
2022-08-29 22:54:36 +0300

    set up

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+using ReactiveUI;
+
+namespace Avalonia.MusicStore.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            BuyMusicCommand() = ReactiveCommand.Create(() =>
+            {
+                
+            });
+        }
+        
+        public ICommand BuyMusicCommand { get; }
+    }
+}
